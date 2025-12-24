854dee56e0266b7e05cec960843cb0d86213d317
ArinaMurashova <arinamur136@mail.ru>
2025-12-24 11:44:48 +0300

    Последовательность JavaScript

---
+++ javascript/posledovatel.js
@@ -0,0 +1,8 @@
+'use strict';
+
+let x = Number(prompt("Введите x = "));
+let y = Number(prompt("Введите y = "));
+
+let z = (x - y)/ Math.sqrt(x = y) + x * Math.pow(y, 2)/Math.sin(Math.pow(x, 2)) * Math.pow(Math.cos(y), 2);
+
+console.log(z);
