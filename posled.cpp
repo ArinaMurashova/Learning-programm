1558d0640d2d657155e755beb3a8167de5da9154
ArinaMurashova <arinamur136@mail.ru>
2025-12-24 10:25:06 +0300

    Последовательность с++

---
+++ posled.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	double x;
+	double y;
+	double z;
+	cout << "Введите x\n";
+	cin >> x;
+
+	cout << "Введите y\n";
+	cin >> y;
+
+	z = (x - y) / sqrt(x + y) + x * pow(y, 2) / sin(pow(x, 2)) * pow(cos(y), 2);
+	cout << z;
+	system("pause");
+}
