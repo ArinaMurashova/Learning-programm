fc1351e007297c85979a6488c5434ec9036d0dd6
ArinaMurashova <arinamur136@mail.ru>
2026-01-22 15:27:45 +0300

    Update SecondPage.tsx

--- react/src/pages/SecondPage.tsx
+++ react/src/pages/SecondPage.tsx
@@ -1,6 +1,174 @@
 const SecondPage = () => {
-    return <div> 
-        SecondPage
+    return <div>
+        <nav>
+            <ul>
+                <li>
+                    Главная
+                </li>
+                <li>
+                    О Кино
+                </li>
+                <li>
+                    Кадры
+                </li>
+                <li>
+                    Персонажи
+                </li>
+            </ul>
+            <button>
+                Поддержка
+            </button>
+        </nav>
+        <header>
+            <article>
+                <h1> STAR WARSEECLIPSE <span> TM </span> </h1>
+                <p> Новый приключенческий экшен с несколькими действующими персонажами и разветвленным повествованием, события которого происходят в эпоху расцвета республики – от Quantic Dream и Lucasfilm Games. Находится сейчас на ранней стадии разработки. </p>
+                <button>
+                    Посмотреть Трейлер
+                </button>
+            </article>
+        </header>
+        <main>
+            <article>
+                <h3> Все еще не можете определиться с выбором кино?</h3>
+                <button> </button>
+                <button> Звёздные войны: Эпизод 1 – Скрытая угроза </button>
+                <button> Звёздные войны: Эпизод 2 – Атака клонов </button>
+                <button> Звёздные войны: Эпизод 3 – Месть ситхов </button>
+                <button> Звёздные войны: Эпизод 4 – Новая надежда </button>
+                <button> Звёздные войны: Эпизод 5 – Империя наносит ответный... </button>
+                <button> </button>
+            </article>
+
+            <figure>
+                <h2> У КАЖДОГО ВЫБОРА ЕСТЬ </h2>
+                <h3> Любое принятое вами решение может иметь драматические последствия для вашего путешествия. Образ жизни на Внешнем кольце находится под угрозой, и вы должны выбрать свой путь. </h3>
+            </figure>
+
+            <figure>
+                <h2> Кадры из кино </h2>
+                <button>
+                    <img alt="hhh" />
+                </button>
+                <button>
+                    <img alt="hhh" />
+                </button>
+                <button>
+                    <img alt="hhh" />
+                </button>
+                <button>
+                    <img alt="hhh" />
+                </button>
+                <button>
+                    <img alt="hhh" />
+                </button>
+                <button>
+                    <img alt="hhh" />
+                </button>
+                <button>
+                    <img alt="hhh" />
+                </button>
+                <button>
+                    <img alt="hhh" />
+                </button>
+                <button>
+                    <img alt="hhh" />
+                </button>
+                <button> Раскрыть Ещё </button>
+            </figure>
+
+            <figure>
+                <h2> ПОСМОТРЕТЬ ТРЕЙЛЕР </h2>
+
+                <figure>
+                    <video />
+                    <button> </button>
+                </figure>
+                <figure>
+                    <video />
+                    <button> </button>
+                </figure>
+                <figure>
+                    <video />
+                    <button> </button>
+                </figure>
+            </figure>
+            <article>
+                <h1> Звёздные Войны </h1>
+                <h2> «Делай до конца. Или не делай совем. Нет такого слова „Пытаться“» </h2>
+                <h5> Мастер Йода</h5>
+            </article>
+
+            <article>
+                <h2> Персонажи </h2>
+                <button> </button>
+                <button>
+                    <img alt="hhh" />
+                    <h3> Дарт Вейдер </h3>
+                    <h5> Центральный персонаж первых шести эпизодов саги «Звёздные войны». Также появляется в фильме «Изгой-один». </h5>
+                </button>
+                <button>
+                    <img alt="hhh" />
+                    <h3> Оби-Ван Кеноби </h3>
+                    <h5> Персонаж медиафраншизы «Звёздные войны». Один из немногих главных героев саги, появлявшихся в той или иной форме в семи фильмах франшизы.   </h5>
+                </button>
+                <button>
+                    <img alt="hhh" />
+                    <h3> Дарт Мол </h3>
+                    <h5> Персонаж медиафраншизы «Звёздные войны», тёмный владыка ситхов, бывший ученик Дарта Сидиуса. </h5>
+                </button>
+                <button>
+                    <img alt="hhh" />
+                    <h3> Асока Тано </h3>
+                    <h5> Бывший падаван Ордена джедаев. Придуман американским мультипликатором и сценаристом Дэйвом Филони. </h5>
+                </button>
+                <button>
+                    <img alt="hhh" />
+                    <h3> Йода </h3>
+                    <h5> Вымышленный персонаж вселенной «Звёздные войны», гранд-мастер ордена джедаев. </h5>
+                </button>
+                <button> </button>
+            </article>
+
+            <article>
+                <h2> Если остались вопросы по "Звездным войнам" мы на них ответим </h2>
+                <h5> «Да пребудет с тобой Сила» </h5>
+                <button> Ваш Вопрос </button>
+                <button> Отправить </button>
+            </article>
+        </main>
+
+        <footer>
+            <figure>
+                <button>
+                    <img alt="hhh" />
+                </button>
+                <button>
+                    <img alt="hhh" />
+                </button>
+                <button>
+                    <img alt="hhh" />
+                </button>
+                <button>
+                    <img alt="hhh" />
+                </button>
+                <button>
+                    <img alt="hhh" />
+                </button>
+                <button>
+                    <img alt="hhh" />
+                </button>
+                <button>
+                    <img alt="hhh" />
+                </button>
+                <button>
+                    <img alt="hhh" />
+                </button>
+                <h3> STAR WOSR ECLIPSE <span> TM </span></h3>
+                <h4> STAR WARS © & ™ Lucasfilm Ltd. All Rights Reserved. © 2023 Quantic Dream.
+                    Quantic Dream and the Quantic Dream logo are trademarks of Quantic Dream. All Rights Reserved. All other marks belong to their respective owners. </h4>
+            </figure>
+        </footer>
     </div>
 }
 
