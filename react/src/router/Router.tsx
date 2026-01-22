47c208bdfaf11bab77f2c454d314fbc27590d754
ArinaMurashova <arinamur136@mail.ru>
2026-01-22 12:15:46 +0300

    react

---
+++ react/src/router/Router.tsx
@@ -0,0 +1,15 @@
+import { Route, Routes } from 'react-router-dom';
+
+import MainPage from '../pages/MainPage';
+import SecondPage from '../pages/SecondPage';
+
+const Router = () => {
+    return (
+        <Routes>
+            <Route path="/" index element={<MainPage />} />
+            <Route path="/aaa" element={<SecondPage />} />
+        </Routes>
+    );
+};
+
+export default Router;
