db8bc63f4ec95fd86537a1d1031dfa2566452965
ArinaMurashova <arinamur136@mail.ru>
2026-03-19 16:13:10 +0300

    Классическое определение вероятности

---
+++ Teorver/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace teorver_f1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            double m = Convert.ToDouble(textBox1.Text);
+
+            double n = Convert.ToDouble(textBox2.Text);
+
+            double p = m / n;
+
+            textBox3.Text = p.ToString();
+
+        }
+    }
+}
